6785a7184e3f7e9328669e55e34b887883d707df
NGUYEN TO DUC TRUONG <269935814+lynxdarealest@users.noreply.github.com>
2026-05-08 15:55:48 +0800

    Delete pixiv directory

--- pixiv/src/chap.js
+++
@@ -1,9 +0,0 @@
-load("pixiv.js");
-
-function execute(url) {
-    var novelId = parseNovelId(url);
-    if (!novelId) return null;
-    var detail = getNovelDetailById(novelId);
-    if (!detail) return null;
-    return Response.success(renderContentHtml(detail));
-}
--- pixiv/src/search.js
+++
@@ -1,6 +0,0 @@
-load("pixiv.js");
-
-function execute(key, page) {
-    var result = getSearchResult(key, page || "1");
-    return Response.success(result.items.map(normalizeNovelCard), result.next);
-}
